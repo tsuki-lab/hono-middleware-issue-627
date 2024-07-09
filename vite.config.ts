c67cb3bb3fc42d94a0621cdf5eef0887f297fc42
hanetsuki <48175599+tsuki-lab@users.noreply.github.com>
2024-07-10 00:09:33 +0900

    remove @hono/vite-cloudflare-pages

--- vite.config.ts
+++ vite.config.ts
@@ -1,6 +1,4 @@
-import build from "@hono/vite-cloudflare-pages";
 import devServer from "@hono/vite-dev-server";
-import adapter from "@hono/vite-dev-server/cloudflare";
 import { defineConfig } from "vite";
 
 export default defineConfig({
@@ -8,9 +6,7 @@ export default defineConfig({
     external: ["react", "react-dom"],
   },
   plugins: [
-    build(),
     devServer({
-      adapter,
       entry: "src/index.tsx",
     }),
   ],
